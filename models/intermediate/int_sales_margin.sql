3d69feae4e7e44e66d1a049d9d8de10f4cdd0e9b
Pierre-Alexandre <pierre.alex19@gmail.com>
2025-07-01 12:34:16 +0000

    Add column margin + purchase_cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,10 @@
+SELECT
+  sales.products_id,
+  sales.quantity,
+  sales.revenue,
+  product.purchase_price,
+  sales.quantity * product.purchase_price AS purchase_cost,
+  ROUND(sales.revenue - (sales.quantity * product.purchase_price),2) AS margin
+FROM {{ ref('stg_raw__sales') }} AS sales
+JOIN {{ ref('stg_raw__product') }} AS product
+  ON sales.products_id = product.products_id

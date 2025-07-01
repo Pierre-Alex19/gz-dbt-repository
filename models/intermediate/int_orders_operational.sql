46cda8f67c98399a66374bb6cc6d751b5f2749d5
Pierre-Alexandre <pierre.alex19@gmail.com>
2025-07-01 12:55:31 +0000

    in_orders_operational

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,21 @@
+SELECT
+  sales.orders_id,
+  sales.date_date,
+  sales.revenue,
+  sales.quantity,
+  sales.quantity * product.purchase_price AS purchase_cost,
+  ROUND(sales.revenue - (sales.quantity * product.purchase_price), 2) AS margin,
+  ROUND(
+    (
+      (sales.revenue - (sales.quantity * product.purchase_price))
+      + CAST(ship.shipping_fee AS FLOAT64)
+      - (CAST(ship.logcost AS FLOAT64) - CAST(ship.ship_cost AS FLOAT64))
+    ),
+    2
+  ) AS operational_margin
+FROM {{ ref('stg_raw__sales') }} AS sales
+JOIN {{ ref('stg_raw__product') }} AS product
+  ON sales.products_id = product.products_id
+JOIN {{ ref('stg_raw__ship') }} AS ship
+  ON sales.orders_id = ship.orders_id
+ORDER BY sales.date_date DESC

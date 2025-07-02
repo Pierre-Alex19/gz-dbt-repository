1ae092aa10f4e092c87250e3fae2b17e56075071
Pierre-Alexandre <pierre.alex19@gmail.com>
2025-07-02 13:45:09 +0000

    update

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,24 +1,30 @@
 SELECT
-  FORMAT_DATE('%Y-%m', f.date_date) AS datemonth,
+  FORMAT_DATE('%Y-%m', COALESCE(f.date_date, c.date_date)) AS datemonth,
 
   -- Métriques publicitaires
-  ROUND(SUM(f.operational_margin - c.total_ads_cost), 2) AS ads_margin,
-  ROUND(AVG(f.total_revenue / NULLIF(f.total_qty_sold, 0)), 2) AS average_basket, -- exemple de panier moyen
-  ROUND(SUM(f.operational_margin), 2) AS operational_margin,
-  ROUND(SUM(c.total_ads_cost), 2) AS ads_cost,
-  SUM(c.total_impressions) AS ads_impression,
-  SUM(c.total_clicks) AS ads_clicks,
+  ROUND(SUM(COALESCE(f.operational_margin, 0) - COALESCE(c.total_ads_cost, 0)), 2) AS ads_margin,
+  ROUND(
+    AVG(
+      COALESCE(f.total_revenue, 0) / NULLIF(COALESCE(f.total_qty_sold, 0), 0)
+    ),
+    2
+  ) AS average_basket,
+  ROUND(SUM(COALESCE(f.operational_margin, 0)), 2) AS operational_margin,
+  ROUND(SUM(COALESCE(c.total_ads_cost, 0)), 2) AS ads_cost,
+  SUM(COALESCE(c.total_impressions, 0)) AS ads_impression,
+  SUM(COALESCE(c.total_clicks, 0)) AS ads_clicks,
 
   -- Métriques financières
-  SUM(f.total_qty_sold) AS quantity,
-  ROUND(SUM(f.total_revenue), 2) AS revenue,
-  ROUND(SUM(f.total_purchase_cost), 2) AS purchase_cost,
-  ROUND(SUM(f.total_revenue - f.total_purchase_cost), 2) AS margin, -- marge brute mensuelle
-  ROUND(SUM(f.total_shipping_fees), 2) AS shipping_fee,
-  ROUND(SUM(f.total_log_costs), 2) AS log_cost
+  SUM(COALESCE(f.total_qty_sold, 0)) AS quantity,
+  ROUND(SUM(COALESCE(f.total_revenue, 0)), 2) AS revenue,
+  ROUND(SUM(COALESCE(f.total_purchase_cost, 0)), 2) AS purchase_cost,
+  ROUND(SUM(COALESCE(f.total_revenue, 0) - COALESCE(f.total_purchase_cost, 0)), 2) AS margin,
+  ROUND(SUM(COALESCE(f.total_shipping_fees, 0)), 2) AS shipping_fee,
+  ROUND(SUM(COALESCE(f.total_log_costs, 0)), 2) AS log_cost
 
 FROM {{ ref('finance_days') }} f
-LEFT JOIN {{ ref('int_campaigns_day') }} c
+FULL OUTER JOIN {{ ref('int_campaigns_day') }} c
   ON f.date_date = c.date_date
+
 GROUP BY datemonth
 ORDER BY datemonth DESC
